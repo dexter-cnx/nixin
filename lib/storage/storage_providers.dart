0dbb0b63567c282b806e7bc04a81a4ff1fb2e9d3
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-17 08:08:17 +0700

    Add storage provider boundary

---
+++ lib/storage/storage_providers.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app_storage.dart';
+
+final appStorageProvider = Provider<AppStorage>((ref) {
+  throw StateError('appStorageProvider must be overridden at application bootstrap');
+});
