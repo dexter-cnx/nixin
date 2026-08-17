4e7a77efea1eae32ac39bff78ed78333f5269286
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-17 08:08:34 +0700

    Bootstrap storage through abstraction

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/nixin_app.dart';
+import 'storage/hive/hive_app_storage.dart';
+import 'storage/storage_providers.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Hive.initFlutter();
-  await Future.wait([
-    Hive.openBox<dynamic>('studio_settings'),
-    Hive.openBox<dynamic>('workplaces'),
-    Hive.openBox<dynamic>('assets'),
-    Hive.openBox<dynamic>('import_batches'),
-  ]);
+  final storage = await HiveAppStorage.open();
 
   runApp(
     ProviderScope(
+      overrides: [
+        appStorageProvider.overrideWithValue(storage),
+      ],
       child: EasyLocalization(
         supportedLocales: const [Locale('en'), Locale('th')],
         path: 'assets/translations',
