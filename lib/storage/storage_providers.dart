2990142e5e4b114f3208377ab4f22ded6486e05e
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-17 08:13:29 +0700

    Format storage abstraction

--- lib/storage/storage_providers.dart
+++ lib/storage/storage_providers.dart
@@ -3,5 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_storage.dart';
 
 final appStorageProvider = Provider<AppStorage>((ref) {
-  throw StateError('appStorageProvider must be overridden at application bootstrap');
+  throw StateError(
+    'appStorageProvider must be overridden at application bootstrap',
+  );
 });
